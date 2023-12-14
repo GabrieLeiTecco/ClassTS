daa47b9d029ede1e2a222dfd5341d7c4af63c066
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2023-12-14 10:36:19 -0300

    Update class.ts

--- class.ts
+++ class.ts
@@ -1,65 +1,72 @@
+// criar uma classe como base pra uma conta no caixaeletronico
+// a classe tem que ter o nome e o saldo da conta
+// e as funções de sacar dinheiro, depositar e listar saldo
+// criar uma função que cria contas, e executa as funções da conta, como um caixaeletronico
+
 class Conta{
     nome: string
     saldo: number
 
-    constructor(nome:string){
+    constructor(nome : string){
         this.nome = nome
         this.saldo = 0
     }
 
-    operacoes(){
-        alert(this.nome+", o que você quer fazer?")
-        let fim = false
-        while(fim == false){
-            let oper = String(prompt("Sacar, depositar, listar saldo ou sair")).toLowerCase()
-            if (oper == 'sacar'){
-                this.sacar()
-                fim = true
-            }else if(oper == 'depositar'){
-                this.depositar()
-                fim = true
-            }else if(oper == 'listar saldo'){
-                this.listar()
-                fim = true
-            }else if(oper == 'sair'){
-                fim = true
-            }
-            else{
-                alert('Sacar, depositar ou listar saldo')
-        }
-        }
-    }
-
     sacar(){
-        let valor = Number(prompt("Quanto você quer sacar? "))
+        let valor : number = Number(prompt('Quanto você quer sacar? '))
         if (valor > this.saldo){
-            alert("Você não tem saldo o suficiente")
-            this.operacoes()
+            alert('Saldo insuficiente.')
         }else{
             this.saldo = this.saldo - valor
-            this.operacoes()
         }
     }
 
     depositar(){
-        let valor = Number(prompt("Quanto você quer depositar? "))
+        let valor : number = Number(prompt('Quanto você quer depositar? '))
         this.saldo = this.saldo + valor
-        this.operacoes()
     }
 
     listar(){
-        alert(this.nome+", seu saldo atual é de: "+this.saldo)
-        this.operacoes()
+        alert(this.nome+', seu saldo atual é de: '+this.saldo)
+    }
+}
+let contas : Array<string> = ['']
+let saldos : Array<number> = [0]
+
+function caixaEletronico(){
+    let conta : any = criarConta()
+    let rodando: boolean = true
+    while(rodando == true){
+    let oper : string = String(prompt('O que você quer fazer? '+'\n'+'(Sacar, depositar, ver saldo ou sair)')).toLowerCase()
+    switch(oper){
+        case 'sacar':
+        conta.sacar()
+        break
+
+        case 'depositar':
+        conta.depositar()
+        break
+
+        case 'ver saldo':
+        conta.listar()
+        break
+
+        case 'sair':
+        rodando = false
+        break
+
+        default:
+        alert('Sacar, depositar ou ver saldo')
+    }
     }
 }
 
-let contas = []
-let conta1 = new Conta('gabriel')
-console.log(conta1.nome)
-contas.push(conta1.nome)
-console.log(contas)
+function criarConta(){
+    let nome : string = String(prompt('Digite seu nome para criarmos uma conta: '))
+    let conta = new Conta(nome)
+    contas.push(conta.nome)
+    saldos.push(conta.saldo)
+    return conta
+}
 
-conta1 = new Conta('Guilherme')
-console.log(conta1.nome)
-contas.push(conta1.nome)
-console.log(contas)
+caixaEletronico()
